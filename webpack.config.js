c80779ec504a20fdab40da3ee4173708f90579c6
fjm <995119598@qq.com>
2017-04-13 14:08:24 +0800

    7

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: "style-loader!css-loader"
       }
     ]
   }
